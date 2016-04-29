76efc8b9d9f5021fa9dc07cae0ec8c514eefc2ae
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-04-29 11:52:46 +0200

    Ajout d'un commentaire

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,6 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
+    // ne faudrait-il pas passer par des constantes ?
     LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
